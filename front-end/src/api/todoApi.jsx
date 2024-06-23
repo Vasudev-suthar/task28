60866695c3140331fd05d7a1ff253f413510226e
Vasudev suthar <doyalvasudev87@gmail.com>
2024-06-23 18:26:27 -0500

    change the api

--- front-end/src/api/todoApi.jsx
+++ front-end/src/api/todoApi.jsx
@@ -1,7 +1,7 @@
 // src/api/todoApi.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks'; // Update with your actual backend URL
+const API_URL = 'https://task28backend.onrender.com/api/tasks'; // Update with your actual backend URL
 
 export const getTasks = () => axios.get(API_URL);
 export const createTask = (task) => axios.post(API_URL, task);
